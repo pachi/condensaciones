aaebb0a8def93335d3a1c07de7385d5165c3234c
Rafael Villar Burke <pachi@rvburke.com>
2009-02-16 13:23:49 +0100

    Añadir comentario y usar unicode en todas las cadenas

--- comprobaciones.py
+++ comprobaciones.py
@@ -22,6 +22,7 @@ def calculafRsimin(tempext, tempint, hrint):
             temp_si_min = 265.5 * math.log (p_sat / 610.5) / (21.875 - math.log (p_sat / 610.5))
         return temp_si_min
     temp_si_min = calculatemp_simin(hrint)
+    #XXX: comprobar si tempint = tempext
     return (temp_si_min - tempext) / (tempint - tempext)
 
 def compruebacsuperificiales(muro, temp_ext, temp_int, HR_int):
@@ -66,10 +67,10 @@ if __name__ == "__main__":
 
     print u"Capas: \n\t", "\n\t".join(muro.nombre_capas)
     print
-    c_sup = compruebacsuperificiales(muro, temp_ext, temp_int, HR_int) and u"Sí" or "No"
+    c_sup = compruebacsuperificiales(muro, temp_ext, temp_int, HR_int) and u"Sí" or u"No"
     print u"Condensaciones superficiales (%s)" % c_sup
     print u"\tfRsi = %.2f, fRsimin = %.2f" % (f_Rsi, f_Rsimin)
-    c_int = compuebacintersticiales(muro, temp_ext, temp_int, HR_ext, HR_int) and u"Sí" or "No"
+    c_int = compuebacintersticiales(muro, temp_ext, temp_int, HR_ext, HR_int) and u"Sí" or u"No"
     print u"Condensaciones intersticiales (%s)" % c_int
 
 #     grafica.dibujapresionestemperaturas("Cerramiento tipo", muro,
