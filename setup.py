8e7a19a31aba35ab75aff22aecad1f95caeae4b8
Rafael Villar Burke <pachi@rvburke.com>
2010-05-10 22:29:15 +0200

    Añadir BBDD de Cerramientos a data_files

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ __version__ = "0.4"
 
 data_files = [('', ['README.txt', 'NEWS.txt', 'INSTALL.txt', 'COPYING.txt']),
               ('data', ['data/condensa.ui', 'data/MaterialesDB.ini',
+                        'data/CerramientoDB.ini',
                         'data/ClimaCTE.ini', 'data/splash.png']),
               ('data/icons', ['data/icons/cerramientos.png',
                               'data/icons/clima.png',
