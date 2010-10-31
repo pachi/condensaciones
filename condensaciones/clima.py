ce3c92b8091ffebbc7cd7bcdf3ed358fc1c56bfc
Rafael Villar Burke <pachi@rvburke.com>
2010-10-31 13:47:34 +0100

    Define API exportable (también para ordenar salida de documentación) y mejora docstring

--- condensaciones/clima.py
+++ condensaciones/clima.py
@@ -20,7 +20,12 @@
 #   along with this program; if not, write to the Free Software
 #   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 #   02110-1301, USA.
-"""Módulo para la definición, almacenamiento y recuperación de ambientes tipo."""
+
+"""Módulo para la definición, almacenamiento y recuperación de ambientes
+higrotérmicos.
+"""
+
+__all__ = ['Clima', 'escape', 'unescape', 'loadclimadb', 'saveclimasdb']
 
 import configobj
 
