c0085307c622629d47bc88eabb11977acaee1b32
Rafael Villar Burke <pachi@rvburke.com>
2015-10-22 13:59:19 +0200

    Simplifica objeto config y añade información de versión

--- condensaciones/util.py
+++ condensaciones/util.py
@@ -22,8 +22,9 @@
 """Módulo de utilidades varias"""
 
 import os, sys, shutil
+from . import __version__
 
-def get_main_dir():
+def find_main_dir():
     """Find main dir even for py2exe frozen modules"""
     if hasattr(sys, "frozen"): #py2exe frozen module
         md = os.path.dirname(sys.executable)
@@ -37,43 +38,44 @@ def get_main_dir():
                 raise ValueError('No se encuentra directorio base')
     return md
 
-class AppConfig(object):
-    def __init__(self):
-        self.maindir = get_main_dir()
-        self.userdir = self._userdir()
-        self.paths = {
-            'mainconf': self.appresource('Condensaciones.ini'),
-            'cerramientosdb': self.userresource('CerramientosDB.ini'),
-            'climasdb': self.userresource('ClimasDB.ini'),
-            'materialesdb': self.userresource('MaterialesDB.ini')
-        }
+def find_or_create_userdir(datadir=None):
+    """Localiza o crea el directorio de configuración y bases de datos
 
-    @property
-    def datadir(self):
-        return os.path.join(self.maindir, 'data')
-            
-    def _userdir(self):
-        """Localiza o crea el directorio de configuración y bases de datos
-
-        Usa por defecto el directorio condensaciones en el home del usario
-        """
-        CONFIGFILES = ['Condensaciones.ini', 'CerramientosDB.ini',
-                       'MaterialesDB.ini', 'ClimasDB.ini']
-        udir = os.path.join(os.path.expanduser('~'), 'condensaciones')
-        if not os.path.exists(udir):
-            os.makedirs(udir)
+    Usa por defecto el directorio condensaciones en el home del usario
+    """
+    CONFIGFILES = ['Condensaciones.ini', 'CerramientosDB.ini',
+                   'MaterialesDB.ini', 'ClimasDB.ini']
+    udir = os.path.join(os.path.expanduser('~'), 'condensaciones')
+    if not os.path.exists(udir):
+        os.makedirs(udir)
+    if datadir and os.path.exists(datadir):
         for cfile in CONFIGFILES:
             ucfile = os.path.join(udir, cfile)
             if not os.path.exists(ucfile):
-                shutil.copy(self.appresource(cfile), ucfile)
-        return udir
+                shutil.copy(os.path.join(datadir, cfile), ucfile)
+    return udir
+
+class AppConfig(object):
+    def __init__(self):
+        maindir = find_main_dir()
+        userdir = find_or_create_userdir(maindir)
+        self.paths = {
+            'maindir': maindir,
+            'datadir': os.path.join(maindir, 'data'),
+            'userdir': userdir,
+            'mainconf': os.path.join(userdir, 'Condensaciones.ini'),
+            'cerramientosdb': os.path.join(userdir, 'CerramientosDB.ini'),
+            'climasdb': os.path.join(userdir, 'ClimasDB.ini'),
+            'materialesdb': os.path.join(userdir, 'MaterialesDB.ini')
+        }
+        self.version = __version__
 
     def appresource(self, *path_list):
         """Localiza un recurso del programa"""
-        return os.path.join(self.datadir, *path_list)
+        return os.path.join(self.paths['datadir'], *path_list)
 
     def userresource(self, *path_list):
         """Localiza un recurso del usuario"""
-        return os.path.join(self.userdir, *path_list)
+        return os.path.join(self.paths['userdir'], *path_list)
 
 config = AppConfig()
